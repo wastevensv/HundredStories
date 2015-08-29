19019798cb4888b4aa17c75b60a96528be4192d3
William Stevens <contact@wastevensv.com>
2015-08-28 22:28:38 -0400

    Pretty much working. Except its not all that pretty yet.

--- stories/urls.py
+++ stories/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+  url(r'^newstory$', views.newstory, name='newstory'),
+  url(r'^write$', views.write, name='write'),
+  url(r'^about$', views.about, name='about'),
   url(r'^(?P<story_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- stories/views.py
+++ stories/views.py
@@ -1,18 +1,36 @@
+import sys
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.utils import timezone
 
 from .models import Story
 
 # Create your views here.
 def index(request):
-  recent_story_list = Story.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('stories/index.html')
+  recent_story_list = Story.objects.order_by('-id')[:5]
   context = RequestContext(request, {
     'recent_story_list': recent_story_list,
   })
-  return HttpResponse(template.render(context))
+  return render(request, 'stories/index.html', context)
 
 def detail(request, story_id):
   story = get_object_or_404(Story, pk=story_id)
+  return render(request, 'stories/detail.html', {'story': story})
+
+def write(request):
+  return render(request, 'stories/write.html')
+
+def about(request):
+  return render(request, 'stories/about.html')
+
+def newstory(request):
+  try:
+    if not request.POST['title']: title = "Untitled"
+    story = Story(title=title, text=request.POST['text'], pub_date=timezone.now())
+  except e:
+    print(e,file=sys.stderr)
+    return HttpResponse("Error Posting Story")
+  story.save()
   return render(request, 'stories/detail.html', {'story': story})
