28d3b9f7cb83f7f018a23bfe469de585705f3e48
William Stevens <contact@wastevensv.com>
2015-08-28 20:03:03 -0400

    Added a basic view and model.

--- HundredStories/urls.py
+++ HundredStories/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from stories import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/', include('stories.urls')),
 ]
---
+++ stories/models.py
@@ -0,0 +1,14 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Story(models.Model):
+  text = models.TextField()
+  title = models.CharField(max_length=200)
+  pub_date = models.DateField('date published')
+  def __str__(self):
+    return self.title
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
